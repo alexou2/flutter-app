215a4f47f726d68e85136b56f45152e5405a7c7e
Alexa <alexandrevalin2@gmail.com>
2026-02-04 20:49:17 -0500

    added popup

--- lib/main.dart
+++ lib/main.dart
@@ -33,10 +33,10 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Colors.pink),
+        colorScheme: .fromSeed(seedColor: Colors.green),
       ),
-      darkTheme: ThemeData.dark(),
 
+      // darkTheme: ThemeData.dark(),
       home: const MyHomePage(title: 'TTRPG app'),
     );
   }
@@ -87,7 +87,7 @@ class _MyHomePageState extends State<MyHomePage> {
   // for checkbox
   bool? checkboxValue = false;
 
-  // equipement levels
+  // weapon skill levels
   Wrapper<double> unarmedXPSpinboxValue = new Wrapper(0);
   Wrapper<double> daggerXPSpinboxValue = new Wrapper(0);
   Wrapper<double> shieldXPSpinboxValue = new Wrapper(0);
@@ -134,13 +134,13 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   // creates a prompt asking for ammount of xp gained
-  double promptForXPGained() {
+  double promptForXPGained(BuildContext context) {
     print("added 10 xp");
-
+_showOverlay(context);
     return 10;
   }
 
-  List<Widget> makeXPSpinboxes<T>(List<(String, Wrapper<double>)> contents) {
+  List<Widget> makeXPSpinboxes(List<(String, Wrapper<double>)> contents, BuildContext context) {
     List<Widget> spinboxList = [];
     for ((String title, Wrapper<double> variable) ee in contents) {
       var spin = Row(
@@ -150,18 +150,20 @@ class _MyHomePageState extends State<MyHomePage> {
               Image.asset("assets/transhaj.png", width: 50, height: 50),
             ],
           ),
+
           Column(
             children: [
               Text(
-                ' ${ee.$2.value!} ${ee.$1} XP (lvl ${pow(2 * ee.$2.value!, (1 / 3)).floor()})',
+                'LEVEL ${pow(2 * ee.$2.value!, (1 / 3)).floor()}  ${ee.$1} (${ee.$2.value!} XP)',
+                // ' ${ee.$2.value!} ${ee.$1} XP (lvl ${pow(2 * ee.$2.value!, (1 / 3)).floor()})',
                 style: TextStyle(fontWeight: FontWeight.bold),
               ),
               TextButton(
                 onPressed: () => setState(() {
-                  var xpGained = promptForXPGained();
+                  var xpGained = promptForXPGained(context);
                   ee.$2.value = ee.$2.value! + xpGained;
                 }),
-                child: Text("add XP"),
+                child: Text("Add XP"),
               ),
             ],
           ),
@@ -171,6 +173,71 @@ class _MyHomePageState extends State<MyHomePage> {
     }
     return spinboxList;
   }
+
+  void _showOverlay(BuildContext context) async {
+    // Declaring and Initializing OverlayState
+    // and OverlayEntry objects
+    OverlayState overlayState = Overlay.of(context);
+    late OverlayEntry overlayEntry;
+
+    overlayEntry = OverlayEntry(
+      builder: (context) {
+        // You can return any widget you like here
+        // to be displayed on the Overlay
+        return Positioned(
+          left: MediaQuery.of(context).size.width * 0.2,
+          top: MediaQuery.of(context).size.height * 0.3,
+          child: Container(
+            width: MediaQuery.of(context).size.width * 0.8,
+            child: Stack(
+              children: [
+                Image.asset(
+                  'assets/transhaj.png',
+                  colorBlendMode: BlendMode.multiply,
+                ),
+                Positioned(
+                  top: MediaQuery.of(context).size.height * 0.15,
+                  left: MediaQuery.of(context).size.width * 0.13,
+                  child: Row(
+                    children: [
+                      Material(
+                        color: Colors.transparent,
+                        child: Text(
+                          'Add XP!',
+                          style: TextStyle(
+                            fontSize:
+                            MediaQuery.of(context).size.height * 0.028,
+                            color: Colors.green,
+                          ),
+                        ),
+                      ),
+                      SizedBox(width: MediaQuery.of(context).size.width * 0.05),
+                      GestureDetector(
+                        onTap: () {
+                          // When the icon is pressed the OverlayEntry
+                          // is removed from Overlay
+                          overlayEntry.remove();
+                        },
+                        child: Icon(
+                          Icons.close,
+                          color: Colors.green,
+                          size: MediaQuery.of(context).size.height * 0.025,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+
+    // Inserting the OverlayEntry into the Overlay
+    overlayState.insert(overlayEntry);
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -235,7 +302,7 @@ class _MyHomePageState extends State<MyHomePage> {
                   ("2-handed haft", h2_haftXPSpinboxValue),
                   ("polearm", polearmXPSpinboxValue),
                   ("sythe", sytheXPSpinboxValue),
-                ]),
+                ], context),
                 mainAxisAlignment: .center,
               ),
             ),
@@ -250,6 +317,16 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(debugShowCheckedModeBanner: false, home: Example1());
+//   }
+// }
+
 
 // children: [
 //   createDropdownMenu(this.input, _numbers_list),
